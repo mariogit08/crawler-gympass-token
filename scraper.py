0ef375469e984898a94117fb0fa5de17542a6e0e
mariogit08 <mario.chaves@live.com>
2018-07-18 22:55:18 -0300

    Incluindo arquivo para login com scrapy

---
+++ scraper.py
@@ -0,0 +1,35 @@
+import scrapy
+import logging
+
+class BrickSetSpider(scrapy.Spider):
+    name = "brickset_spider"
+    start_urls = ['https://www.gympass.com/']
+
+    def parse(self, response):
+        return scrapy.Request('https://www.gympass.com/pessoas/entrar',
+                          callback=self.parse_loggin)
+    
+    def parse_loggin(self, response):      
+        person_search_info = '09812092463'
+        person_type = 'b2b'
+        commit = 'Continuar'       
+        utf8 = response.xpath('//input[@name="utf8"]/@value').get()
+        self.log(utf8)        
+        authenticity_token = response.xpath('//input[@name="authenticity_token"]/@value').get()
+        self.log(authenticity_token)
+        url = 'https://www.gympass.com//pessoas/entrar?utf8=%E2%9C%93&authenticity_token='+authenticity_token+'&person_search_info=09812092463&person_type=b2b&commit=Continuar?'
+        self.log(url)
+        return scrapy.Request(url,
+                          callback=self.parse_senha)
+    
+    def parse_senha(self, response):
+        url = 'https://www.gympass.com/pessoas/entrar'        
+        utf8 = '%E2%9C%93'
+        authenticity_token = response.xpath('//input[@name="authenticity_token"]/@value').get()                
+        person[email]: 'mario.chaves@live.com'
+        person[password]: 'Gym051943'
+        person[remember_me]: '0'
+        person[remember_me]: '1'                        
+        self.log(authenticity_token)
+        url = 'https://www.gympass.com/pessoas/entrar'
+        self.log(url)
